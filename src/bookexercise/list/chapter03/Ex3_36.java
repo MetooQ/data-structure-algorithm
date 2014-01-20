d43d108034bb9089f4651d231b280b43820a121c
qiudeqing <deqing.qiu@gmail.com>
2014-01-20 08:21:18 +0800

    delete current node

---
+++ src/bookexercise/list/chapter03/Ex3_36.java
@@ -0,0 +1,80 @@
+package bookexercise.list.chapter03;
+
+import bookexercise.list.chapter03.Ex3_36.LinkedList.Node;
+
+
+public class Ex3_36 {
+  
+  public static void main(String[] args) {
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    int i = 0;
+    
+    for (i = 0; i < 10; ++i) {
+      list.push(i);
+    } // end for
+    System.out.println(list);
+    
+    Node<Integer> node = list.get(3);
+    list.deleteNode(node);
+    System.out.println(list);
+  }
+  
+  static class LinkedList<T> {
+    private Node<T> header;
+    
+    public LinkedList() {} // end LinkedList()
+    
+    @Override
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      Node<T> current = header;
+      
+      sb.append("[");
+      if (current != null) {
+        sb.append(current.data);
+        current = current.next;
+      } // end if
+      
+      while (current != null) {
+        sb.append("," + current.data);
+        current = current.next;
+      } // end while
+      
+      sb.append("]");
+      return sb.toString();
+    } // end toString()
+    
+    public void push(T e) {
+      Node<T> node = new Node<T>(e, header);
+      header = node;
+    } // end push()
+    
+    public Node<T> get(int index) {
+      Node<T> current = header;
+      while (index > 0) {
+        --index;
+        current = current.next;
+      } // end while
+      return current;
+    } // end get
+    
+    public T deleteNode(Node<T> node) {
+      T value = node.data;
+      node.data = node.next.data;
+      node.next = node.next.next;
+      return value;
+    } // end deleteNode()
+    
+    static class Node<T> {
+      public T data;
+      public Node<T> next;
+      
+      public Node(){}
+      public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+      } // end Node()
+    } // end class Node
+  } // end class LinkedList
+
+}
