8ba24e17bc252455b4b2f78c439704d0e5292a8d
qiudeqing <deqing.qiu@gmail.com>
2014-02-25 08:50:40 +0800

    quicksort

--- src/bookexercise/sort/QuickSort2.java
+++ src/bookexercise/sort/QuickSort2.java
@@ -2,18 +2,19 @@ package bookexercise.sort;
 
 public class QuickSort2 {
   public static final int CUTOFF = 10; // 数组小于10个元素，采用插入排序。更快。
+  public static final int SIZE = 50;
   
   public static void main(String[] args) {
-    Integer[] data = new Integer[50];
+    Integer[] data = new Integer[SIZE];
     int i;
     
     
-    for (i = 0; i < 50; ++i) {
-      data[i] = 50 - i;
+    for (i = 0; i < SIZE; ++i) {
+      data[i] = SIZE - i;
     } // end for
     
     QuickSort2.sort(data);
-    for (i = 0; i < 50; ++i) {
+    for (i = 0; i < SIZE; ++i) {
       System.out.printf("%d ", data[i]);
     }
   } // end main()
@@ -28,40 +29,35 @@ public class QuickSort2 {
     int i, j;
     T mid;
     
-    if (left + QuickSort2.CUTOFF < right) {
-      mid = mediun(data, left, right);
+    if (left + CUTOFF < right) {
+      mid = medium(data, left, right);
       i = left;
       j = right - 1;
-      while (true) {
+      
+      while (true){
         while (data[++i].compareTo(mid) < 0) {}
         while (data[--j].compareTo(mid) > 0) {}
         if (i < j) {
           swap(data, i, j);
-        } // end if
-        else {
+        } else {
           break;
-        } // end else
+        }
       } // end while
       
       swap(data, i, right - 1);
       quicksort(data, left, i - 1);
-      quicksort(data, i + 1, right);
-    } // end if
+      quicksort(data, left + 1, right);
+      
+    } // quicksort
     else {
-      insertSort(data, left, right);
-    } // end else
+      insertsort(data, left, right);
+    } // insert sort
   } // end quicksort()
   
-  /**
-   * 对left， center， right排序保证循环出口，交换中值与right - 1方便后续交换
-   * @param data
-   * @param left
-   * @param right
-   * @return left, center, right的中值
-   */
   public static <T extends Comparable<? super T>>
-  T mediun(T[] data, int left, int right) {
+  T medium(T[] data, int left, int right) {
     int center = (left + right) / 2;
+    
     if (data[left].compareTo(data[center]) > 0) {
       swap(data, left, center);
     }
@@ -75,25 +71,24 @@ public class QuickSort2 {
     return data[right - 1];
   } // end medium()
   
-  public static <T>
-  void swap(T[] data, int i, int j) {
+  public static <T> void swap(T[] data, int i, int j) {
     T temp = data[i];
     data[i] = data[j];
     data[j] = temp;
-  } // end swap()
+  }
   
   public static <T extends Comparable<? super T>>
-  void insertSort(T[] data, int left, int right) {
+  void insertsort(T[] data, int left, int right) {
     int i, j;
     T temp;
+    
     for (i = left + 1; i <= right; ++i) {
       temp = data[i];
-      for (j = i; j > 0 && temp.compareTo(data[j - 1]) < 0; --j) {
+      for (j = i; j > left && temp.compareTo(data[j - 1]) < 0; --j) {
         data[j] = data[j - 1];
       } // end for
       data[j] = temp;
     } // end for
-  } // end insertSort()
-  
+  } // end insertsort()
   
 } // end class QuickSort
