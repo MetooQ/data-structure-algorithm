ea87155d3abf444f7c71ff060dea55ad88bb52e8
qiudeqing <deqing.qiu@gmail.com>
2014-02-27 16:14:53 +0800

    update

--- src/leetcode/Candy.java
+++ src/leetcode/Candy.java
@@ -4,27 +4,32 @@ public class Candy {
 
   static class Solution {
     public int candy(int[] ratings) {
-      int i, sum = 0, current = 1, 
-          len = ratings.length,
+      int sum = 0, i = 1, len = ratings.length,
           cmp;
-        
-      
+      int[] candies = new int[len];
+
       if (len == 1) {
         return 1;
-      } // end if
-
-      sum = current = 1;
+      }
+      
+      candies[0] = 1;
       for (i = 1; i < len; ++i) {
-          cmp = ratings[i] - ratings[i - 1];
-          if (cmp > 0) {
-            ++current;
-          } else if (cmp < 0) {
-            --current;
-            current = current <= 0 ? 1 : current;
-          }
-          sum += current;
+        cmp = ratings[i] - ratings[i - 1];
+        if (cmp > 0) {
+          candies[i] = candies[i - 1] + 1;
+        } else {
+          candies[i] = 1;
+        }
       } // end for
       
+      sum = candies[len - 1];
+      for (i = len - 2; i >= 0; --i) {
+        if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+          candies[i] = candies[i + 1] + 1;
+        }
+        sum += candies[i];
+      } // end for
+
       return sum;
     }
   } // end Solution
